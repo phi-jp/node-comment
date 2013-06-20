555777ccd219eb346abe6f6868a695161ac2f51c
phi <phi1618jp@gmail.com>
2013-06-20 20:25:40 +0900

    fix

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+// app.set('view engine', 'ejs');
+// app.set('view engine', 'jshtml-express');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
