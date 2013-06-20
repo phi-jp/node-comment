356cc414125605c430e14866a4a0e373996f4625
phi <phi1618jp@gmail.com>
2013-06-20 21:11:25 +0900

    support hjs

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 // app.set('view engine', 'ejs');
-// app.set('view engine', 'jshtml-express');
+// app.set('view engine', 'hjs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
